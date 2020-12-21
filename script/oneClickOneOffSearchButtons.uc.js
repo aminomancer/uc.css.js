a98e5da575221e3042053b5af33b5f78450f07db
aminomancer <33384265+aminomancer@users.noreply.github.com>
2020-12-21 14:19:20 -0800

    fix

--- script/oneClickOneOffSearchButtons.uc.js
+++ script/oneClickOneOffSearchButtons.uc.js
@@ -1,1 +1,129 @@
-function searchClickInit(){function e(e){for(let n of e)switch(n.type){case"childList":case"subtree":for(let e=0;e<s.length;e++)"true"!=s[e].getAttribute("listening")&&(s[e].addEventListener("click",t,!1),s[e].setAttribute("listening","true"))}}function t(e){if(2==e.button)return;let t=e.originalTarget;(t.engine||t.source)&&(r.selectedButton=t,n(e,{engineName:t.engine.name,source:t.source,entry:"oneoff"}),e.stopImmediatePropagation(),e.preventDefault())}function n(e,t){if(e.target==r.settingsButtonCompact)return r.input.controller.engagementEvent.discard(),void r.selectedButton.doCommand();let n={allowAutofill:!t.engineName&&t.source!=UrlbarUtils.RESULT_SOURCE.SEARCH,event:e},i=r.input.value&&"valid"!=r.input.getAttribute("pageproxystate"),s=Services.search.getEngineByName(t.engineName),{where:a,params:o}=r._whereToOpen(e);if(i&&s)return r.input.handleNavigation({event:e,oneOffParams:{openWhere:a,openParams:o,engine:r.selectedButton.engine}}),void(r.selectedButton=null);switch(a){case"current":r.input.searchMode=t,r.input.startQuery(n);break;case"tab":{t.isPreview=!1;let e=r.input.window.gBrowser.addTrustedTab("about:newtab");r.input.setSearchMode(t,e.linkedBrowser),i&&(e.linkedBrowser.userTypedValue=r.input.value),o.inBackground||(r.input.window.gBrowser.selectedTab=e,e.ownerGlobal.gURLBar.startQuery(n));break}default:r.input.searchMode=t,r.input.startQuery(n),r.input.select()}r.selectedButton=null}var r=gURLBar.view.oneOffSearchButtons,i=r.buttons,s=i.children;let a=new MutationObserver(e),o={childList:!0,subtree:!0};a.observe(i,o)}if(gBrowserInit.delayedStartupFinished)this.searchClickInit();else{let e=(t,n)=>{"browser-delayed-startup-finished"==n&&t==window&&(Services.obs.removeObserver(e,n),this.searchClickInit())};Services.obs.addObserver(e,"browser-delayed-startup-finished")}
+function searchClickInit() {
+    var oneOffs = gURLBar.view.oneOffSearchButtons,
+        buttons = oneOffs.buttons,
+        buttonsList = buttons.children;
+    let observer = new MutationObserver(listenAll),
+        options = {
+            childList: true,
+            subtree: true,
+        };
+
+    observer.observe(buttons, options);
+
+    function listenAll(mutations) {
+        for (let mutation of mutations) {
+            switch (mutation.type) {
+                case "childList":
+                case "subtree":
+                    // setTimeout(() => {
+                    for (let i = 0; i < buttonsList.length; i++) {
+                        if (buttonsList[i].getAttribute("listening") != "true") {
+                            buttonsList[i].addEventListener("click", searchClickCallback, false);
+                            buttonsList[i].setAttribute("listening", "true");
+                        }
+                    }
+                    // }, 100);
+                    break;
+            }
+        }
+    }
+
+    function searchClickCallback(event) {
+        if (event.button == 2) {
+            return;
+        }
+
+        let button = event.originalTarget;
+        if (!button.engine && !button.source) {
+            return;
+        }
+
+        oneOffs.selectedButton = button;
+        executeSearchOnClick(event, {
+            engineName: button.engine.name,
+            source: button.source,
+            entry: "oneoff",
+        });
+        event.stopImmediatePropagation();
+        event.preventDefault();
+    }
+
+    function executeSearchOnClick(event, searchMode) {
+        // The settings button is a special case. Its action should be executed
+        // immediately.
+        if (event.target == oneOffs.settingsButtonCompact) {
+            oneOffs.input.controller.engagementEvent.discard();
+            oneOffs.selectedButton.doCommand();
+            return;
+        }
+
+        let startQueryParams = {
+                allowAutofill: !searchMode.engineName && searchMode.source != UrlbarUtils.RESULT_SOURCE.SEARCH,
+                event,
+            },
+            userTypedSearchString = oneOffs.input.value && oneOffs.input.getAttribute("pageproxystate") != "valid",
+            engine = Services.search.getEngineByName(searchMode.engineName),
+            { where, params } = oneOffs._whereToOpen(event);
+        // { where, params } = oneOffs.input._whereToOpen(event);
+
+        if (userTypedSearchString && engine) {
+            oneOffs.input.handleNavigation({
+                event,
+                oneOffParams: {
+                    openWhere: where,
+                    openParams: params,
+                    engine: oneOffs.selectedButton.engine,
+                },
+            });
+            oneOffs.selectedButton = null;
+            return;
+        }
+
+        switch (where) {
+            case "current": {
+                oneOffs.input.searchMode = searchMode;
+                oneOffs.input.startQuery(startQueryParams);
+                break;
+            }
+            case "tab": {
+                // We set this.selectedButton when switching tabs. If we entered search
+                // mode preview here, it could be cleared when this.selectedButton calls
+                // setSearchMode.
+                searchMode.isPreview = false;
+
+                let newTab = oneOffs.input.window.gBrowser.addTrustedTab("about:newtab");
+                oneOffs.input.setSearchMode(searchMode, newTab.linkedBrowser);
+                if (userTypedSearchString) {
+                    // Set the search string for the new tab.
+                    newTab.linkedBrowser.userTypedValue = oneOffs.input.value;
+                }
+                if (!params.inBackground) {
+                    oneOffs.input.window.gBrowser.selectedTab = newTab;
+                    newTab.ownerGlobal.gURLBar.startQuery(startQueryParams);
+                }
+                break;
+            }
+            default: {
+                oneOffs.input.searchMode = searchMode;
+                oneOffs.input.startQuery(startQueryParams);
+                oneOffs.input.select();
+                break;
+            }
+        }
+
+        oneOffs.selectedButton = null;
+    }
+}
+
+// Delayed startup
+if (gBrowserInit.delayedStartupFinished) {
+    this.searchClickInit();
+} else {
+    let delayedStartupFinished = (subject, topic) => {
+        if (topic == "browser-delayed-startup-finished" && subject == window) {
+            Services.obs.removeObserver(delayedStartupFinished, topic);
+            this.searchClickInit();
+        }
+    };
+    Services.obs.addObserver(delayedStartupFinished, "browser-delayed-startup-finished");
+}
