c790821c1b267a9cb0b05c9ab6b3445519cd2c11
aminomancer <33384265+aminomancer@users.noreply.github.com>
2021-01-27 23:48:11 -0800

    update agentsheets

--- script/agentsheets.uc.js
+++ script/agentsheets.uc.js
@@ -1,11 +1,9 @@
 // ==UserScript==
-// @name           userChrome.js
-// @namespace      scrollbars_win10
-// @version        0.0.8
-// @note           Windows 10 style by /u/mrkwatz https://www.reddit.com/r/FirefoxCSS/comments/7fkha6/firefox_57_windows_10_uwp_style_overlay_scrollbars/
-// @note           Brought to Firefox 57 by /u/Wiidesire https://www.reddit.com/r/firefox/comments/7f6kc4/floating_scrollbar_finally_possible_in_firefox_57/
-// @note           userChrome.js https://github.com/nuchi/firefox-quantum-userchromejs
-// @note           Forked from https://github.com/Endor8/userChrome.js/blob/master/floatingscrollbar/FloatingScrollbar.uc.js
+// @name           agentsheets.uc.js
+// @version        1.0
+// @homepage       https://github.com/aminomancer
+// @description    Make Windows scrollbars look like macOS scrollbars. Also hide the tab bar's scroll buttons and overflow end indicators.
+// @author         aminomancer
 // ==/UserScript==
 
 (function () {
