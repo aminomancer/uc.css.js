9647179dea9b03222cf95a43e908440ee96990b5
aminomancer <33384265+aminomancer@users.noreply.github.com>
2021-02-07 20:10:54 -0800

    remove unnecessary font rules

--- script/bookmarksPopupShadowRoot.uc.js
+++ script/bookmarksPopupShadowRoot.uc.js
@@ -1,8 +1,8 @@
 (function () {
     function init () {
-        var BMBtoolbarPopup = document.getElementById('BMB_bookmarksToolbarPopup');
-        var BMBinnerBox = document.getElementById('BMB_bookmarksToolbarPopup').shadowRoot.children[1];
-        var BMBarrowScrollbox = document.getElementById('BMB_bookmarksToolbarPopup').shadowRoot.children[1].children[1];
+        const BMBtoolbarPopup = document.getElementById('BMB_bookmarksToolbarPopup');
+        const BMBinnerBox = document.getElementById('BMB_bookmarksToolbarPopup').shadowRoot.children[1];
+        const BMBarrowScrollbox = document.getElementById('BMB_bookmarksToolbarPopup').shadowRoot.children[1].children[1];
 
         BMBinnerBox.classList.add("BMB-special-innerbox");
         BMBarrowScrollbox.smoothScroll = true;
@@ -26,13 +26,13 @@
 
         function toolbarPopupOpened() {
             try {
-                var bmbSubviewButtons = document.getElementsByClassName('menu-iconic bookmark-item subviewbutton');
+                const bmbSubviewButtons = document.getElementsByClassName('menu-iconic bookmark-item subviewbutton');
                 BMBinnerBox.classList.add("BMB-special-innerbox");
                 BMBarrowScrollbox.smoothScroll = true;
                 BMBarrowScrollbox._scrollIncrement = 150;
                 BMBarrowScrollbox._scrollButtonUp.classList.add("BMB-special-scrollbutton-up");
                 BMBarrowScrollbox._scrollButtonDown.classList.add("BMB-special-scrollbutton-down");
-                for (var i = 0, max = bmbSubviewButtons.length; i < max; i++) {
+                for (let i = 0, max = bmbSubviewButtons.length; i < max; i++) {
                     if (bmbSubviewButtons[i].getElementsByTagName('menupopup')[0].shadowRoot.querySelector('hbox[part="innerbox"]').clientHeight < 700) {
                         bmbSubviewButtons[i].getElementsByTagName('menupopup')[0].shadowRoot.querySelector('hbox[part="innerbox"]').classList.add("BMBsmallContentBox");
                     }
