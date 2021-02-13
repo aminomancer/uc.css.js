1005cc078a4a2430eb3e2f30aaac746a16a8e6dc
aminomancer <33384265+aminomancer@users.noreply.github.com>
2021-02-12 16:56:53 -0800

    major visual update cleaning context menus up updates for #identity-icon-box

--- script/searchSelectionShortcut.uc.js
+++ script/searchSelectionShortcut.uc.js
@@ -6,11 +6,54 @@
 // ==/UserScript==
 
 (() => {
-    const frameScript = `const{Services:Services}=ChromeUtils.import("resource://gre/modules/Services.jsm"),{XPCOMUtils:XPCOMUtils}=ChromeUtils.import("resource://gre/modules/XPCOMUtils.jsm");XPCOMUtils.defineLazyModuleGetters(this,{E10SUtils:"resource://gre/modules/E10SUtils.jsm",SelectionUtils:"resource://gre/modules/SelectionUtils.jsm"});let attachKeyListener=(e,t)=>{if("document-element-inserted"==t&&content&&e==content.document)try{content.addEventListener("keydown",(e=>{if("KeyF"===e.code&&e.ctrlKey&&e.shiftKey&&!e.repeat){try{let t=SelectionUtils.getSelectionDetails(content);if(t.text&&!t.docSelectionIsCollapsed){let s=e.composedTarget.ownerDocument.nodePrincipal,i={csp:E10SUtils.serializeCSP(e.composedTarget.ownerDocument.csp),text:t.text,linkURL:t.linkURL,principal:s};try{this.sendAsyncMessage("ctrl-shift-f",i)}catch(e){}}}catch(e){}e.stopPropagation(),e.stopImmediatePropagation(),e.preventDefault()}}))}catch(e){}};Services.obs.addObserver(attachKeyListener,"document-element-inserted"),attachKeyListener(content.document,"document-element-inserted");`;
+    function frameScript() {
+        const { Services: Services } = ChromeUtils.import("resource://gre/modules/Services.jsm"),
+            { XPCOMUtils: XPCOMUtils } = ChromeUtils.import(
+                "resource://gre/modules/XPCOMUtils.jsm"
+            ),
+            utils = {};
+        XPCOMUtils.defineLazyModuleGetters(utils, {
+            E10SUtils: "resource://gre/modules/E10SUtils.jsm",
+            SelectionUtils: "resource://gre/modules/SelectionUtils.jsm",
+        });
+        let attachKeyListener = (e, t) => {
+            if ("document-element-inserted" == t && content && e == content.document)
+                try {
+                    content.addEventListener("keydown", (e) => {
+                        if ("KeyF" === e.code && e.ctrlKey && e.shiftKey && !e.repeat) {
+                            try {
+                                let t = utils.SelectionUtils.getSelectionDetails(content);
+                                if (t.text && !t.docSelectionIsCollapsed) {
+                                    let s = e.composedTarget.ownerDocument.nodePrincipal,
+                                        i = {
+                                            csp: utils.E10SUtils.serializeCSP(
+                                                e.composedTarget.ownerDocument.csp
+                                            ),
+                                            text: t.text,
+                                            linkURL: t.linkURL,
+                                            principal: s,
+                                        };
+                                    try {
+                                        sendAsyncMessage("ctrl-shift-f", i);
+                                    } catch (e) {}
+                                }
+                            } catch (e) {}
+                            e.stopPropagation(), e.stopImmediatePropagation(), e.preventDefault();
+                        }
+                    });
+                } catch (e) {}
+        };
+        Services.obs.addObserver(attachKeyListener, "document-element-inserted"),
+            attachKeyListener(content.document, "document-element-inserted");
+    }
 
     function init() {
         try {
-            window.messageManager.loadFrameScript(`data:,${frameScript}`, true);
+            window.messageManager.loadFrameScript(
+                "data:application/javascript," +
+                    encodeURIComponent(`(${frameScript.toString()})()`),
+                true
+            );
         } catch (e) {}
         window.messageManager.addMessageListener("ctrl-shift-f", (message) => {
             if (message.target === gBrowser.selectedBrowser) {
