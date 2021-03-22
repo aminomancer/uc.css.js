910f0112d6d7b4b2c9c1e3f6c0ca7fcca175c9fb
aminomancer <33384265+aminomancer@users.noreply.github.com>
2021-03-22 03:21:02 -0700

    all tabs menu: style multiselect & container tabs fix fullscreen nav bar bug

--- script/allTabsMenuDimUnloadedTabs.uc.js
+++ script/allTabsMenuDimUnloadedTabs.uc.js
@@ -1,50 +1,79 @@
 // ==UserScript==
 // @name           Dim unloaded tabs in the "allTabsMenu"
 // @homepage       https://github.com/aminomancer
-// @description    Next to the "new tab" button in Firefox there's a V-shaped button that opens a big scrolling menu containing all the tabs. By default, Firefox doesn't do anything to differentiate loaded tabs from unloaded tabs. But for the regular tab bar, unloaded tabs gain an attribute pending="true" which you can use to dim them. This way you know which tabs are already initialized and which will actually start up when you click them. Pretty useful if you frequently have 100+ tabs like me. But the "all tabs" menu doesn't give tabs the same attribute, so CSS alone can't distinguish running tabs from unloaded or "discarded" tabs. We use a lightweight script to calculate each tab's loadedness upon receiving events that are likely to load/unload a tab, and set an attribute on each tab accordingly, with which we can style the tabs in CSS: .all-tabs-item[pending] {opacity: .6}
+// @description    Next to the "new tab" button in Firefox there's a V-shaped button that opens a big scrolling menu containing all the tabs. By default, Firefox doesn't do anything to differentiate loaded tabs from unloaded tabs. But for the regular tab bar, unloaded tabs gain an attribute pending="true" which you can use to dim them. This way you know which tabs are already initialized and which will actually start up when you click them. Pretty useful if you frequently have 100+ tabs like me. But the "all tabs" menu doesn't give tabs the same attribute, so CSS alone can't distinguish running tabs from unloaded or "discarded" tabs. We use a lightweight script to calculate each tab's loadedness upon receiving events that are likely to load/unload a tab, and set an attribute on each tab accordingly, with which we can style the tabs in CSS: .all-tabs-item[pending] {opacity: .6} This now additionally adds attributes to multiselected tabs and container tabs. You can use these attributes to style the tabs too. See the end of uc-tabs-bar.css for an example where I've added a color stripe to indicate multiselected or container tabs.
 // @author         aminomancer
 // ==/UserScript==
 (function () {
-    let tab,
-        timer,
-        observer = new MutationObserver(delayedChange),
-        options = {
-            attributes: true,
-            attributeFilter: ["pending", "busy"],
-        };
+    let tab;
+    let timer;
+    let tabContext = document.getElementById("tabContextMenu");
+    let observer = new MutationObserver(delayedUpdate);
 
-    function contextCmd(e) {
+    function contextCmd(_e) {
         observer.disconnect();
         if (gTabsPanel.allTabsPanel.view.panelMultiView) {
             tab = TabContextMenu.contextTab;
-            observer.observe(tab, options);
+            observer.observe(tab, {
+                attributes: true,
+                attributeFilter: ["pending", "busy"],
+            });
         }
     }
 
-    function delayedChange(mus) {
-        for (let mu of mus) {
-            setOpacity();
-            window.clearTimeout(timer);
-            timer = window.setTimeout(() => {
-                observer.disconnect();
-            }, 3000);
+    function tabUpdated(_e) {
+        if (gTabsPanel.allTabsPanel.view.panelMultiView) {
+            updateTabItems();
         }
     }
 
-    function setOpacity() {
-        let tabs = gTabsPanel.allTabsViewTabs.children;
-        for (let i = 0; i < tabs.length; i++) {
-            tabs[i].tab.linkedPanel
-                ? tabs[i].removeAttribute("pending")
-                : tabs[i].setAttribute("pending", "true");
+    function contextHide(_e) {
+        if (gTabsPanel.allTabsPanel.view.panelMultiView) delayedDisconnect();
+    }
+
+    function delayedUpdate(mus) {
+        for (const _mu of mus) {
+            updateTabItems();
+            delayedDisconnect();
         }
+    }
+
+    function delayedDisconnect() {
+        window.clearTimeout(timer);
+        timer = window.setTimeout(() => {
+            observer.disconnect();
+        }, 3000);
+    }
+
+    function updateTabItems() {
+        Array.from(gTabsPanel.allTabsViewTabs.children).forEach((item) => {
+            item.tab.linkedPanel
+                ? item.removeAttribute("pending")
+                : item.setAttribute("pending", "true");
+            item.tab.multiselected
+                ? item.setAttribute("multiselected", "true")
+                : item.removeAttribute("multiselected");
+            if (item.tab.userContextId) {
+                let idColor = ContextualIdentityService.getPublicIdentityFromId(
+                    item.tab.userContextId
+                )?.color;
+                item.className = `all-tabs-item identity-color-${idColor}`;
+                item.setAttribute("usercontextid", item.tab.userContextId);
+            } else {
+                item.className = "all-tabs-item";
+                item.removeAttribute("usercontextid");
+            }
+        });
     }
 
     function start() {
         setTimeout(() => {
             gTabsPanel.init();
-            gTabsPanel.allTabsView.addEventListener("ViewShowing", setOpacity);
-            document.getElementById("tabContextMenu").addEventListener("command", contextCmd, true);
+            gTabsPanel.allTabsView.addEventListener("ViewShowing", updateTabItems);
+            tabContext.addEventListener("command", contextCmd, true);
+            tabContext.addEventListener("popuphidden", contextHide, false);
+            gBrowser.tabContainer.addEventListener("TabAttrModified", tabUpdated, false);
+            gBrowser.addEventListener("TabMultiSelect", tabUpdated, false);
         }, 1000);
     }
 
--- script/fullscreenNavBar.uc.js
+++ script/fullscreenNavBar.uc.js
@@ -7,110 +7,65 @@
 
 (() => {
     function init() {
-        var mainPopupSet = document.getElementById("mainPopupSet");
-        var navBar = document.getElementById("navigator-toolbox");
-        var navBarContainer = navBar.parentElement;
-        var urlbar = document.getElementById("urlbar");
-        var backButton = document.getElementById("back-button");
-        var fwdButton = document.getElementById("forward-button");
-        const options = {
-            attributes: true,
-            attributeFilter: ["focused"],
-        };
-        const observer = new MutationObserver(urlbarCallback);
+        const mainPopupSet = document.getElementById("mainPopupSet");
+        const navBar = document.getElementById("navigator-toolbox");
+        const navBarContainer = navBar.parentElement;
+        const urlbar = document.getElementById("urlbar");
+        const backButton = document.getElementById("back-button");
+        const fwdButton = document.getElementById("forward-button");
 
-        function popupShowing(event) {
-            switch (event.target.id) {
-                case "contentAreaContextMenu":
-                case "aHTMLTooltip":
-                case "remoteBrowserTooltip":
-                case "sidebarMenu-popup":
-                case "ctrlTab-panel":
-                case "dynamic-shortcut-tooltip":
-                case "SyncedTabsSidebarContext":
-                case "SyncedTabsSidebarTabsFilterContext":
-                case "urlbar-scheme":
-                case "urlbar-input":
-                case "urlbar-label-box":
-                case "urlbar-search-mode-indicator":
-                case "tracking-protection-icon-tooltip":
-                case "tabbrowser-tab-tooltip":
-                case "pageActionContextMenu":
-                case "confirmation-hint":
-                    break;
-                case "backForwardMenu":
-                    if (backButton.disabled == true && fwdButton.disabled == true) {
-                        break;
-                    } else {
-                        navBarContainer.setAttribute("popup-status", "true");
-                    }
-                case "":
-                    if (event.target.hasAttribute("menu-api")) {
-                        break;
-                    }
-                default:
-                    navBarContainer.setAttribute("popup-status", "true");
-            }
-        }
-
-        function popupHiding(event) {
-            switch (event.target.id) {
-                case "contentAreaContextMenu":
-                case "aHTMLTooltip":
-                case "remoteBrowserTooltip":
-                case "sidebarMenu-popup":
-                case "ctrlTab-panel":
-                case "dynamic-shortcut-tooltip":
-                case "SyncedTabsSidebarContext":
-                case "SyncedTabsSidebarTabsFilterContext":
-                case "urlbar-scheme":
-                case "urlbar-input":
-                case "urlbar-label-box":
-                case "urlbar-search-mode-indicator":
-                case "tracking-protection-icon-tooltip":
-                case "tabbrowser-tab-tooltip":
-                case "pageActionContextMenu":
-                case "confirmation-hint":
-                    break;
-                case "backForwardMenu":
-                    if (backButton.disabled == true && fwdButton.disabled == true) {
-                        break;
-                    } else {
-                        navBarContainer.removeAttribute("popup-status");
-                    }
-                case "":
-                    if (event.target.hasAttribute("menu-api")) {
-                        break;
-                    }
-                default:
-                    if (event.target.className != "urlbarView") {
-                        if (
-                            event.target.parentElement.tagName != "menu" ||
-                            event.target.parentElement.parentElement.tagName == "menubar"
-                        ) {
-                            navBarContainer.removeAttribute("popup-status");
-                        }
-                    }
-            }
-        }
-
-        function urlbarCallback(mutationsList, observer) {
-            for (let mutation of mutationsList) {
-                if (mutation.type === "attributes") {
-                    if (urlbar.getAttribute("focused") == "true") {
-                        navBarContainer.setAttribute("urlbar-status", "true");
-                    } else {
-                        navBarContainer.removeAttribute("urlbar-status");
-                    }
+        const fullscreenHandler = {
+            handleEvent(event) {
+                if (event.target.tagName === "tooltip") return;
+                switch (event.target.id) {
+                    case "contentAreaContextMenu":
+                    case "sidebarMenu-popup":
+                    case "ctrlTab-panel":
+                    case "SyncedTabsSidebarContext":
+                    case "SyncedTabsSidebarTabsFilterContext":
+                    case "urlbar-scheme":
+                    case "urlbar-input":
+                    case "urlbar-label-box":
+                    case "urlbar-search-mode-indicator":
+                    case "pageActionContextMenu":
+                    case "confirmation-hint":
+                        return;
+                    case "backForwardMenu":
+                        if (backButton.disabled && fwdButton.disabled) return;
+                    case "":
+                        if (event.target.hasAttribute("menu-api")) return;
                 }
-            }
-        }
+                switch (event.type) {
+                    case "popupshowing":
+                        navBarContainer.setAttribute("popup-status", true);
+                        break;
+                    case "popuphiding":
+                        if (event.target.className === "urlbarView") return;
+                        if (
+                            event.target.parentElement.tagName === "menu" &&
+                            event.target.parentElement.parentElement.tagName !== "menubar"
+                        )
+                            return;
+                        navBarContainer.removeAttribute("popup-status");
+                        break;
+                }
+            },
 
-        observer.observe(urlbar, options);
-        mainPopupSet.addEventListener("popupshowing", popupShowing, true);
-        mainPopupSet.addEventListener("popuphiding", popupHiding, true);
-        navBar.addEventListener("popupshowing", popupShowing, true);
-        navBar.addEventListener("popuphiding", popupHiding, true);
+            urlbarCallback(mutationsList, _observer) {
+                for (const _mutation of mutationsList) {
+                    if (urlbar.getAttribute("focused"))
+                        navBarContainer.setAttribute("urlbar-status", true);
+                    else navBarContainer.removeAttribute("urlbar-status");
+                }
+            },
+        };
+        const observer = new MutationObserver(fullscreenHandler.urlbarCallback);
+
+        observer.observe(urlbar, { attributes: true, attributeFilter: ["focused"] });
+        mainPopupSet.addEventListener("popupshowing", fullscreenHandler, true);
+        mainPopupSet.addEventListener("popuphiding", fullscreenHandler, true);
+        navBar.addEventListener("popupshowing", fullscreenHandler, true);
+        navBar.addEventListener("popuphiding", fullscreenHandler, true);
     }
 
     if (gBrowserInit.delayedStartupFinished) {
