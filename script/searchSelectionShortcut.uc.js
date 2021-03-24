2d83a8cac26de2379758f7209b1ff883d599b655
aminomancer <33384265+aminomancer@users.noreply.github.com>
2021-03-24 02:24:08 -0700

    major improvement to search selection shortcut it's now much better at determining if selection is url won't parse nonsense like "this.is.not.a.url" as a url also fixed some context menu checkboxes

--- script/searchSelectionShortcut.uc.js
+++ script/searchSelectionShortcut.uc.js
@@ -7,11 +7,11 @@
 
 (() => {
     function frameScript() {
-        const { Services: Services } = ChromeUtils.import("resource://gre/modules/Services.jsm"),
-            { XPCOMUtils: XPCOMUtils } = ChromeUtils.import(
-                "resource://gre/modules/XPCOMUtils.jsm"
-            ),
-            utils = {};
+        const utils = {};
+        const { Services: Services } = ChromeUtils.import("resource://gre/modules/Services.jsm");
+        const { XPCOMUtils: XPCOMUtils } = ChromeUtils.import(
+            "resource://gre/modules/XPCOMUtils.jsm"
+        );
         XPCOMUtils.defineLazyModuleGetters(utils, {
             E10SUtils: "resource://gre/modules/E10SUtils.jsm",
             SelectionUtils: "resource://gre/modules/SelectionUtils.jsm",
@@ -30,7 +30,7 @@
                                         ),
                                         text: s.text,
                                         linkURL: s.linkURL,
-                                        location: content.location.href,
+                                        locationURL: content.location.href,
                                     };
                                     try {
                                         sendAsyncMessage("ctrl-shift-f", msg);
@@ -42,8 +42,8 @@
                     });
                 } catch (e) {}
         };
-        Services.obs.addObserver(attachKeyListener, "document-element-inserted"),
-            attachKeyListener(content.document, "document-element-inserted");
+        Services.obs.addObserver(attachKeyListener, "document-element-inserted");
+        attachKeyListener(content.document, "document-element-inserted");
     }
 
     function init() {
@@ -57,31 +57,53 @@
         window.messageManager.addMessageListener("ctrl-shift-f", (message) => {
             if (message.target === gBrowser.selectedBrowser) {
                 try {
-                    let csp = E10SUtils.deserializeCSP(message.data.csp),
-                        text = message.data.text,
-                        linkURL = message.data.linkURL,
-                        principal = Services.scriptSecurityManager.getSystemPrincipal(),
-                        location = message.data.location,
-                        where =
-                            location === AboutNewTab.newTabURL || location === HomePage.get(window)
-                                ? "current"
-                                : "tab";
+                    let csp = E10SUtils.deserializeCSP(message.data.csp);
+                    let text = message.data.text;
+                    let linkURL = message.data.linkURL;
+                    let principal = Services.scriptSecurityManager.getSystemPrincipal();
+                    let locationURL = message.data.locationURL;
+                    let options = { inBackground: false, triggeringPrincipal: principal };
+                    let where =
+                        locationURL === AboutNewTab.newTabURL ||
+                        locationURL === HomePage.get(window)
+                            ? "current"
+                            : "tab";
+
+                    if (
+                        text.startsWith("chrome://") ||
+                        text.startsWith("file://") ||
+                        text.startsWith("about:")
+                    )
+                        return openLinkIn(text, where, options);
                     if (linkURL) {
-                        openLinkIn(linkURL, where, {
-                            inBackground: false,
-                            triggeringPrincipal: principal,
-                        });
-                    } else {
-                        window.BrowserSearch._loadSearch(
-                            text,
-                            where,
-                            false,
-                            "contextmenu",
-                            principal,
-                            csp,
-                            false
-                        );
+                        let fixup, fixable;
+                        try {
+                            fixup = Services.uriFixup.getFixupURIInfo(
+                                text,
+                                Services.uriFixup.FIXUP_FLAG_ALLOW_KEYWORD_LOOKUP
+                            );
+                            fixable = true;
+                        } catch (e) {
+                            fixable = false;
+                        }
+                        if (fixable && !fixup._keywordProviderName) {
+                            linkURL =
+                                fixup._fixedURI.scheme === "http"
+                                    ? fixup._fixedURI.host
+                                    : fixup._fixedURI.spec;
+                            if (linkURL) return openLinkIn(linkURL, where, options);
+                        }
                     }
+
+                    window.BrowserSearch._loadSearch(
+                        text,
+                        where,
+                        false,
+                        "contextmenu",
+                        principal,
+                        csp,
+                        false
+                    );
                 } catch (e) {}
             }
         });
