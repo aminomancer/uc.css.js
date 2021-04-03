2cae37b1ad2d3da18fff821fe0afb494b3532849
aminomancer <33384265+aminomancer@users.noreply.github.com>
2021-04-03 01:32:27 -0700

    fix a minor findbar bug merge agent sheet and author sheet scripts improve the devtools sheet loader script. should be more selective now.

--- script/userChrome_as_css_module.uc.js
+++ script/userChrome_as_css_module.uc.js
@@ -1,31 +1,39 @@
 // ==UserScript==
-// @name           userChrome_as_css_module.uc.js
-// @namespace      userChrome_Agent_Sheet_CSS_module
-// @version        0.0.5
-// @note           Load userChrome.as.css file as agent sheet
+// @name           Agent/Author Sheet Loader
+// @namespace      userChrome_Agent_Author_Sheet_CSS_module
+// @version        2.0
+// @note           Load userChrome.as.css file as agent sheet and userChrome.au.css file as author sheet
 // @backgroundmodule
 // ==/UserScript==
 
 let EXPORTED_SYMBOLS = [];
 (function () {
-  const {Services} = ChromeUtils.import('resource://gre/modules/Services.jsm');
-	let sss = Cc['@mozilla.org/content/style-sheet-service;1'].getService(Ci.nsIStyleSheetService);
-  function traverseToMainProfile(str){
-    let dir = Services.dirsvc.get(str,Ci.nsIFile);
-    if (!dir.exists()) {
-      let toAddChrome = false;
-      while (dir.target.includes('chrome_debugger_profile')) {
-        dir = dir.parent;
-        toAddChrome = true;
-      }
-      if (toAddChrome) dir.append('chrome');
+    const { Services } = ChromeUtils.import("resource://gre/modules/Services.jsm");
+    let sss = Cc["@mozilla.org/content/style-sheet-service;1"].getService(Ci.nsIStyleSheetService);
+    function traverseToMainProfile(str) {
+        let dir = Services.dirsvc.get(str, Ci.nsIFile);
+        if (!dir.exists()) {
+            let toAddChrome = false;
+            while (dir.target.includes("chrome_debugger_profile")) {
+                dir = dir.parent;
+                toAddChrome = true;
+            }
+            if (toAddChrome) dir.append("chrome");
+        }
+        return dir;
     }
-    return dir;
-  }
-  const path = Services.io.getProtocolHandler('file').QueryInterface(Ci.nsIFileProtocolHandler).getURLSpecFromDir(traverseToMainProfile('UChrm'));
-  try{
-    sss.loadAndRegisterSheet(Services.io.newURI(path + "userChrome.as.css"), sss.AGENT_SHEET);
-  }catch(e){
-    console.error(`Could not load userChrome.as.css: ${e.name}`)
-  }
+    const path = Services.io
+        .getProtocolHandler("file")
+        .QueryInterface(Ci.nsIFileProtocolHandler)
+        .getURLSpecFromDir(traverseToMainProfile("UChrm"));
+    try {
+        sss.loadAndRegisterSheet(Services.io.newURI(path + "userChrome.as.css"), sss.AGENT_SHEET);
+    } catch (e) {
+        console.error(`Could not load userChrome.as.css: ${e.name}`);
+    }
+    try {
+        sss.loadAndRegisterSheet(Services.io.newURI(path + "userChrome.au.css"), sss.AUTHOR_SHEET);
+    } catch (e) {
+        console.error(`Could not load userChrome.au.css: ${e.name}`);
+    }
 })();
--- script/userChrome_devtools_module.uc.js
+++ script/userChrome_devtools_module.uc.js
@@ -1,33 +1,64 @@
 // ==UserScript==
-// @name           userChrome_devtools_module.uc.js
+// @name           Browser Toolbox Stylesheet Loader
 // @namespace      userChrome_DevTools_module
-// @version        1.0
+// @version        2.0
 // @note           Load userChrome and userContent stylesheets into Browser Toolbox windows
 // @backgroundmodule
 // ==/UserScript==
 
 let EXPORTED_SYMBOLS = [];
 (function () {
-  const {Services} = ChromeUtils.import('resource://gre/modules/Services.jsm');
-  if (!Services.dirsvc.get('UChrm',Ci.nsIFile).target.includes('chrome_debugger_profile')) return;
-	let sss = Cc['@mozilla.org/content/style-sheet-service;1'].getService(Ci.nsIStyleSheetService);
-  function traverseToMainProfile(str){
-    let dir = Services.dirsvc.get(str,Ci.nsIFile);
-    if (!dir.exists()) {
-      let toAddChrome = false;
-      while (dir.target.includes('chrome_debugger_profile')) {
-        dir = dir.parent;
-        toAddChrome = true;
-      }
-      if (toAddChrome) dir.append('chrome');
+    const { Services } = ChromeUtils.import("resource://gre/modules/Services.jsm");
+    class ToolboxProcessSheetLoader {
+        constructor() {
+            Services.obs.addObserver(this, "domwindowopened", false);
+            this.regex = /^chrome:(\/\/devtools\/.*.html.*)/i;
+            this.lastSubject = null;
+        }
+        traverseToMainProfile(win, str) {
+            let dir = Services.dirsvc.get(str, win.Ci.nsIFile);
+            if (!dir.exists()) {
+                let toAddChrome = false;
+                while (dir.target.includes("chrome_debugger_profile")) {
+                    dir = dir.parent;
+                    toAddChrome = true;
+                }
+                if (toAddChrome) dir.append("chrome");
+            }
+            return dir;
+        }
+        loadSheet(win, path, name, type) {
+            let sss = win.Cc["@mozilla.org/content/style-sheet-service;1"].getService(
+                win.Ci.nsIStyleSheetService
+            );
+            try {
+                sss.loadAndRegisterSheet(win.Services.io.newURI(path + name), sss[type]);
+            } catch (e) {}
+        }
+        observe(sub) {
+            if (this.lastSubject === sub) return;
+            this.lastSubject = sub;
+            sub.addEventListener("DOMContentLoaded", this, true);
+        }
+        handleEvent(e) {
+            let document = e.originalTarget;
+            let win = document.defaultView;
+            this.lastSubject.removeEventListener("DomContentLoaded", this, true);
+            if (
+                !Services.dirsvc
+                    .get("UChrm", Ci.nsIFile)
+                    .target.includes("chrome_debugger_profile") ||
+                !this.regex.test(win.location.href)
+            )
+                return;
+            const path = Services.io
+                .getProtocolHandler("file")
+                .QueryInterface(win.Ci.nsIFileProtocolHandler)
+                .getURLSpecFromDir(this.traverseToMainProfile(win, "UChrm"));
+            this.loadSheet(win, path, "userChrome.css", "AGENT_SHEET");
+            this.loadSheet(win, path, "userContent.css", "USER_SHEET");
+        }
     }
-    return dir;
-  }
-  const path = Services.io.getProtocolHandler('file').QueryInterface(Ci.nsIFileProtocolHandler).getURLSpecFromDir(traverseToMainProfile('UChrm'));
-  try{
-    sss.loadAndRegisterSheet(Services.io.newURI(path + "userChrome.css"), sss.AGENT_SHEET);
-    sss.loadAndRegisterSheet(Services.io.newURI(path + "userContent.css"), sss.USER_SHEET);
-  }catch(e){
-    console.error(`Could not load stylesheets into Browser Toolbox: ${e.name}`)
-  }
+
+    new ToolboxProcessSheetLoader();
 })();
