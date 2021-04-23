ea1b1e3676eb8b1d49bb9e408481cc27c8f7c4fc
aminomancer <33384265+aminomancer@users.noreply.github.com>
2021-04-22 23:13:10 -0700

    bookmark popup styles and scrolling script: now applies to the menubar's bookmarks popup too.

--- script/bookmarksPopupShadowRoot.uc.js
+++ script/bookmarksPopupShadowRoot.uc.js
@@ -1,99 +1,28 @@
 const bookmarksPopupShadowRoot = {
-    init() {
-        const BMBtoolbarPopup = document.getElementById("BMB_bookmarksToolbarPopup");
-        const BMBarrowScrollbox = BMBtoolbarPopup.scrollBox;
+    handleEvent(e) {
+        if (!e.target.getAttribute("uc-init"))
+            setTimeout(() => {
+                this.checkPopups(e.target);
+            }, 0);
+    },
 
-        function setUpScroll() {
-            BMBtoolbarPopup.shadowRoot.children[1].classList.add("BMB-special-innerbox");
-            BMBarrowScrollbox.smoothScroll = true;
-            BMBarrowScrollbox._scrollIncrement = 150;
-            BMBarrowScrollbox._scrollButtonUp.classList.add("BMB-special-scrollbutton-up");
-            BMBarrowScrollbox._scrollButtonDown.classList.add("BMB-special-scrollbutton-down");
-        }
+    checkPopups(popup) {
+        popup.setAttribute("uc-init", true);
+        let scrollbox = popup.scrollBox.scrollbox;
+        let height = window.screen.availHeight;
+        if (scrollbox.scrollTopMax < height && scrollbox.clientHeight < height)
+            popup.shadowRoot.querySelector(`hbox`)?.classList.add("BMBsmallContentBox");
+        else this.setUpScroll(popup);
+    },
 
-        function toolbarPopupOpened() {
-            try {
-                document
-                    .getElementById("BMB_bookmarksPopup")
-                    .querySelectorAll(`menupopup[placespopup="true"]`)
-                    .forEach((popup) => {
-                        let scrollbox = popup.scrollBox.scrollbox;
-                        let height = window.screen.availHeight;
-                        if (scrollbox.scrollTopMax < height && scrollbox.clientHeight < height)
-                            popup.shadowRoot
-                                .querySelector('hbox[part="innerbox"]')
-                                .classList.add("BMBsmallContentBox");
-                    });
+    setUpScroll(popup) {
+        popup.shadowRoot?.querySelector(`hbox`).classList.add("BMB-special-innerbox");
+        popup.scrollBox.smoothScroll = true;
+        popup.scrollBox._scrollIncrement = 150;
+        popup.scrollBox._scrollButtonUp.classList.add("BMB-special-scrollbutton-up");
+        popup.scrollBox._scrollButtonDown.classList.add("BMB-special-scrollbutton-down");
 
-                setUpScroll();
-
-                for (let i = 0, max = bmbSubviewButtons.length; i < max; i++) {
-                    if (
-                        bmbSubviewButtons[i]
-                            .getElementsByTagName("menupopup")[0]
-                            .shadowRoot.querySelector('hbox[part="innerbox"]').clientHeight < 700
-                    ) {
-                        bmbSubviewButtons[i]
-                            .getElementsByTagName("menupopup")[0]
-                            .shadowRoot.querySelector('hbox[part="innerbox"]')
-                            .classList.add("BMBsmallContentBox");
-                    }
-                }
-            } catch (e) {}
-        }
-
-        function scrollByIndex(box, index, aInstant) {
-            if (index == 0) {
-                return;
-            }
-
-            var rect = box.scrollClientRect;
-            var [start, end] = box.startEndProps;
-            var x = index > 0 ? rect[end] + 1 : rect[start] - 1;
-            var nextElement = box._elementFromPoint(x, index);
-            if (!nextElement) {
-                return;
-            }
-
-            var targetElement;
-            if (box.isRTLScrollbox) {
-                index *= -1;
-            }
-            while (index < 0 && nextElement) {
-                if (box._canScrollToElement(nextElement)) {
-                    targetElement = nextElement;
-                }
-                nextElement = nextElement.previousElementSibling;
-                index++;
-            }
-            while (index > 0 && nextElement) {
-                if (box._canScrollToElement(nextElement)) {
-                    targetElement = nextElement;
-                }
-                nextElement = nextElement.nextElementSibling;
-                index--;
-            }
-
-            if (!targetElement || !box._canScrollToElement(targetElement)) return;
-
-            box._stopScroll();
-
-            let animFrame = window.requestAnimationFrame(() => {
-                targetElement.scrollIntoView({
-                    block: "nearest",
-                    behavior: aInstant ? "instant" : "auto",
-                });
-                box._ensureElementIsVisibleAnimationFrame = 0;
-                box._arrowScrollAnim.requestHandle = 0;
-            });
-
-            box._ensureElementIsVisibleAnimationFrame = animFrame;
-            box._arrowScrollAnim.requestHandle = animFrame;
-        }
-
-        setUpScroll();
-
-        BMBarrowScrollbox._onButtonMouseOver = function _onButtonMouseOver(index) {
+        popup.scrollBox._onButtonMouseOver = function _onButtonMouseOver(index) {
             if (this._ensureElementIsVisibleAnimationFrame || this._arrowScrollAnim.requestHandle)
                 return;
             if (this._clickToScroll) {
@@ -103,7 +32,7 @@ const bookmarksPopupShadowRoot = {
             }
         };
 
-        BMBarrowScrollbox._onButtonMouseOut = function _onButtonMouseOut() {
+        popup.scrollBox._onButtonMouseOut = function _onButtonMouseOut() {
             if (this._ensureElementIsVisibleAnimationFrame || this._arrowScrollAnim.requestHandle)
                 return;
             if (this._clickToScroll) {
@@ -113,15 +42,67 @@ const bookmarksPopupShadowRoot = {
             }
         };
 
-        BMBarrowScrollbox._scrollButtonDown.onclick = function scrollToBottom() {
-            scrollByIndex(BMBarrowScrollbox, BMBtoolbarPopup.children.length);
+        popup.scrollBox._scrollButtonDown.onclick = function scrollToBottom() {
+            bookmarksPopupShadowRoot.scrollByIndex(popup.scrollBox, popup.children.length);
         };
 
-        BMBarrowScrollbox._scrollButtonUp.onclick = function scrollToTop() {
-            scrollByIndex(BMBarrowScrollbox, -BMBtoolbarPopup.children.length);
+        popup.scrollBox._scrollButtonUp.onclick = function scrollToTop() {
+            bookmarksPopupShadowRoot.scrollByIndex(popup.scrollBox, -popup.children.length);
         };
+    },
 
-        BMBtoolbarPopup.addEventListener("popupshowing", toolbarPopupOpened, false, { once: true });
+    scrollByIndex(box, index, aInstant) {
+        if (index == 0) {
+            return;
+        }
+
+        var rect = box.scrollClientRect;
+        var [start, end] = box.startEndProps;
+        var x = index > 0 ? rect[end] + 1 : rect[start] - 1;
+        var nextElement = box._elementFromPoint(x, index);
+        if (!nextElement) {
+            return;
+        }
+
+        var targetElement;
+        if (box.isRTLScrollbox) {
+            index *= -1;
+        }
+        while (index < 0 && nextElement) {
+            if (box._canScrollToElement(nextElement)) {
+                targetElement = nextElement;
+            }
+            nextElement = nextElement.previousElementSibling;
+            index++;
+        }
+        while (index > 0 && nextElement) {
+            if (box._canScrollToElement(nextElement)) {
+                targetElement = nextElement;
+            }
+            nextElement = nextElement.nextElementSibling;
+            index--;
+        }
+
+        if (!targetElement || !box._canScrollToElement(targetElement)) return;
+
+        box._stopScroll();
+
+        let animFrame = window.requestAnimationFrame(() => {
+            targetElement.scrollIntoView({
+                block: "nearest",
+                behavior: aInstant ? "instant" : "auto",
+            });
+            box._ensureElementIsVisibleAnimationFrame = 0;
+            box._arrowScrollAnim.requestHandle = 0;
+        });
+
+        box._ensureElementIsVisibleAnimationFrame = animFrame;
+        box._arrowScrollAnim.requestHandle = animFrame;
+    },
+
+    init() {
+        document.getElementById("BMB_bookmarksPopup").addEventListener("popupshowing", this, true);
+        document.getElementById("bookmarksMenuPopup").addEventListener("popupshowing", this, true);
         CustomizableUI.removeListener(this);
     },
 
@@ -132,25 +113,26 @@ const bookmarksPopupShadowRoot = {
 
     onWindowClosed(aWindow) {
         try {
-            aWindow === window
-                ? CustomizableUI.removeListener(this)
-                : CustomizableUI.removeListener(aWindow.bookmarksPopupShadowRoot);
+            CustomizableUI.removeListener(aWindow.bookmarksPopupShadowRoot);
         } catch (e) {}
     },
+
     addHandler() {
         if (document.getElementById("bookmarks-menu-button")) this.init();
         else CustomizableUI.addListener(this);
     },
 };
 
-if (gBrowserInit.delayedStartupFinished) {
-    bookmarksPopupShadowRoot.addHandler();
-} else {
-    let delayedListener = (subject, topic) => {
-        if (topic == "browser-delayed-startup-finished" && subject == window) {
-            Services.obs.removeObserver(delayedListener, topic);
-            bookmarksPopupShadowRoot.addHandler();
-        }
-    };
-    Services.obs.addObserver(delayedListener, "browser-delayed-startup-finished");
-}
+(function () {
+    if (gBrowserInit.delayedStartupFinished) {
+        bookmarksPopupShadowRoot.addHandler();
+    } else {
+        let delayedListener = (subject, topic) => {
+            if (topic == "browser-delayed-startup-finished" && subject == window) {
+                Services.obs.removeObserver(delayedListener, topic);
+                bookmarksPopupShadowRoot.addHandler();
+            }
+        };
+        Services.obs.addObserver(delayedListener, "browser-delayed-startup-finished");
+    }
+})();
