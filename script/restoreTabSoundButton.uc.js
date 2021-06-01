8a52e0f2b1ebd58e68ce348a534ac3d4a21d50c3
aminomancer <33384265+aminomancer@users.noreply.github.com>
2021-06-01 16:09:41 -0700

    restore tab sound button: use a better method, and use the normal tab tooltip for the sound button.

--- script/restoreTabSoundButton.uc.js
+++ script/restoreTabSoundButton.uc.js
@@ -1,94 +1,26 @@
 // ==UserScript==
 // @name           Restore pre-Proton Tab Sound Button
-// @version        1.0
+// @version        1.1
 // @author         aminomancer
 // @homepage       https://github.com/aminomancer/uc.css.js
-// @description    Proton makes really big changes to tabs, in particular removing the tab sound button. We can currently restore it to some extent with CSS, but it won't display a tooltip and it won't work when the video playing in the tab is in picture-in-picture mode. We can't really restore these functions, we have to recreate them. So this script generates a localized tooltip for the sound button based on its current function (mute, unmute, or block) and also makes the sound button work in picture-in-picture mode. As you might expect, it will mute the tab if it's not muted, and unmute the tab if it is already muted. This is consistent with the styling in uc-tabs.css: in PiP mode, the sound button is styled to look like a PiP icon, but when hovering the tab it will transform into a regular mute/unmute button, since the PiP button has no functionality of its own, it's purely a visual indicator.
+// @description    Proton makes really big changes to tabs, in particular removing the tab sound button in favor of the overlay button. We can currently restore it to some extent with CSS, but it won't display a tooltip and it won't work when the video playing in the tab is in picture-in-picture mode. This script keeps the new tab tooltip enabled by the pref "browser.proton.places-tooltip.enabled" but allows it to work with the old .tab-icon-sound. So you get the nice parts of the proton tab changes without the second row of text about the audio playing. Instead it will show the mute/unmute tooltip inside the normal tab tooltip. It also changes the tooltip a bit so that it's always anchored to the tab rather than floating around tethered to the exact mouse position. This makes it easier to modify the tooltip appearance without the tooltip getting in your way.
 // ==/UserScript==
 
-(function () {
-    const tabEventHandler = {
-        handleEvent(e) {
-            if (!e.target.classList.contains("tab-icon-sound")) return;
-            let tab = e.target.closest(".tabbrowser-tab");
-            switch (e.type) {
-                case "mouseover":
-                case "mouseout":
-                    this.tooltipHandler(e, tab);
-                    break;
-                case "click":
-                    this.clickHandler(e, tab);
-                    break;
-                default:
-                    return false;
-            }
+(async function () {
+    let css = `.tab-icon-sound-label,.tab-secondary-label{display:none;}.tab-label-container:not(.proton) + .tab-icon-sound{display:-moz-box;}.tab-icon-sound:not([soundplaying],[muted],[activemedia-blocked],[pictureinpicture]),.tab-icon-sound[pinned]{display:none;}.tab-icon-overlay{display:none;}`;
+    let sss = Cc["@mozilla.org/content/style-sheet-service;1"].getService(Ci.nsIStyleSheetService);
+    let uri = makeURI("data:text/css;charset=UTF=8," + encodeURIComponent(css));
+    if (!sss.sheetRegistered(uri, sss.AUTHOR_SHEET))
+        sss.loadAndRegisterSheet(uri, sss.AUTHOR_SHEET);
+    let tabClass = await customElements.whenDefined("tabbrowser-tab");
+    eval(
+        `gBrowser.createTooltip = function ` +
+            gBrowser.createTooltip.toSource().replace(/alignToTab \= false\;\n/g, "")
+    );
+    Object.defineProperty(tabClass.prototype, "soundPlayingIcon", {
+        get() {
+            return this.querySelector(".tab-icon-sound");
         },
-
-        stringWithShortcut(stringId, keyElemId, pluralCount) {
-            let keyElem = document.getElementById(keyElemId);
-            let shortcut = ShortcutUtils.prettifyShortcut(keyElem);
-            return PluralForm.get(pluralCount, gTabBrowserBundle.GetStringFromName(stringId))
-                .replace("%S", shortcut)
-                .replace("#1", pluralCount);
-        },
-
-        async tooltipHandler(e, tab) {
-            const selectedTabs = gBrowser.selectedTabs;
-            const tabInSelection = selectedTabs.includes(tab);
-            const affectedTabsLength = tabInSelection ? selectedTabs.length : 1;
-
-            if (tab.selected)
-                label = this.stringWithShortcut(
-                    tab.linkedBrowser.audioMuted
-                        ? "tabs.unmuteAudio2.tooltip"
-                        : "tabs.muteAudio2.tooltip",
-                    "key_toggleMute",
-                    affectedTabsLength
-                );
-            else
-                label = PluralForm.get(
-                    affectedTabsLength,
-                    gTabBrowserBundle.GetStringFromName(
-                        tab.hasAttribute("activemedia-blocked")
-                            ? "tabs.unblockAudio2.tooltip"
-                            : tab.linkedBrowser.audioMuted
-                            ? "tabs.unmuteAudio2.background.tooltip"
-                            : "tabs.muteAudio2.background.tooltip"
-                    )
-                ).replace("#1", affectedTabsLength);
-
-            e.target.setAttribute("tooltiptext", label);
-        },
-
-        clickHandler(e, tab) {
-            if (e.button != 0 || e.getModifierState("Accel") || e.shiftKey) return;
-
-            if (tab.soundPlaying || tab.muted || tab.activeMediaBlocked) {
-                tab.multiselected
-                    ? gBrowser.toggleMuteAudioOnMultiSelectedTabs(tab)
-                    : tab.toggleMuteAudio();
-                e.preventDefault();
-                e.stopPropagation();
-                return;
-            }
-        },
-
-        attachListeners() {
-            gBrowser.tabContainer.addEventListener("mouseover", this, true);
-            gBrowser.tabContainer.addEventListener("mouseout", this, true);
-            gBrowser.tabContainer.addEventListener("click", this, true);
-        },
-    };
-
-    if (gBrowserInit.delayedStartupFinished) {
-        tabEventHandler.attachListeners();
-    } else {
-        let delayedListener = (subject, topic) => {
-            if (topic == "browser-delayed-startup-finished" && subject == window) {
-                Services.obs.removeObserver(delayedListener, topic);
-                tabEventHandler.attachListeners();
-            }
-        };
-        Services.obs.addObserver(delayedListener, "browser-delayed-startup-finished");
-    }
+    });
+    gBrowser.tabContainer.addEventListener("click", this, true);
 })();
