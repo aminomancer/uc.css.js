1e61ac258ac3cfefcc8c61200c3bfae70b966544
aminomancer <33384265+aminomancer@users.noreply.github.com>
2021-06-15 14:48:53 -0700

    update readme

--- script/removeSearchEngineAliasFormatting.uc.js
+++ script/removeSearchEngineAliasFormatting.uc.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name           Remove Search Engine Alias Formatting
-// @version        1.0
+// @version        1.1
 // @author         aminomancer
 // @homepage       https://github.com/aminomancer
 // @description    Depending on your settings you might have noticed that typing a search engine alias (e.g. "goo" for Google) causes some special formatting to be applied to the text you input in the url bar. This is a trainwreck because the formatting is applied using the selection controller, not via CSS, meaning you can't change it in your stylesheets. It's blue by default, and certainly doesn't match my personal theme very well. This script just prevents the formatting from ever happening at all.
@@ -9,13 +9,8 @@
 (() => {
     function startup() {
         function init() {
-            if (!gURLBar.valueFormatter._formatSearchAlias) {
-                return;
-            }
-            gURLBar.valueFormatter._formatSearchAlias = function () {
-                try {
-                } catch (e) {}
-            };
+            if (!gURLBar.valueFormatter._formatSearchAlias) return;
+            gURLBar.valueFormatter._formatSearchAlias = () => false;
             gURLBar.removeEventListener("focus", init);
         }
 
