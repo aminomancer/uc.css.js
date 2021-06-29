962dcfc745942c155be8bd4a08fa3ac77bdbf1d4
aminomancer <33384265+aminomancer@users.noreply.github.com>
2021-06-29 03:32:00 -0700

    fix problems with pinned tabs and tab animations; allow moving all selected tabs with ctrl+shift+pg up/down: (miscMods.uc.js); fix some font inconsistencies.

--- script/miscMods.uc.js
+++ script/miscMods.uc.js
@@ -1,31 +1,83 @@
 // ==UserScript==
 // @name           Misc. Mods
-// @version        1.0
+// @version        1.1
 // @author         aminomancer
 // @homepage       https://github.com/aminomancer/uc.css.js
 // @description    Various tiny mods not worth making separate scripts for. Read the comments inside the script for details.
 // ==/UserScript==
 
-
 (function () {
-    function init() {
+    let config = {
         // by default the bookmarks toolbar unhides itself when you use the edit bookmark panel and select the bookmarks toolbar as the bookmark's folder. this is super annoying so I'm completely turning it off.
-        gEditItemOverlay._autoshowBookmarksToolbar = function () {};
+        "Disable bookmarks toolbar auto show": true,
+
         // on macOS the arrow keyboard shortcuts (cmd+shift+pgup) "wrap" relative to the tab bar, so moving the final tab right will move it to the beginning of the tab bar. for some reason this is turned off on linux and windows. I'm turning it on.
-        gBrowser.arrowKeysShouldWrap = true;
+        "Moving tabs with arrow keys can wrap": true,
+
         // for some reason, when you open the downloads panel it automatically focuses the first element, which is the footer if you don't have any downloads. this is inconsistent with other panels, and a little annoying imo. it's not a big deal but one of firefox's biggest problems compared to other browsers is a general lack of consistency. so I think removing this whole behavior would probably be wise, but for now I'll just stop it from focusing the *footer*, but still allow it to focus the first download item if there are any.
-        eval(
-            `DownloadsPanel._focusPanel = function ` +
-                DownloadsPanel._focusPanel.toSource().replace(/DownloadsFooter\.focus\(\)\;/, ``)
-        );
+        "Stop downloads panel auto-focusing the footer button": true,
+
+        // when you use the "move tab" hotkeys, e.g. Ctrl + Shift + PageUp, it only moves the active tab, even if you have multiple tabs selected. this is inconsistent with the keyboard shortcuts "move tab to end" or "move tab to start" and of course, inconsistent with the drag & drop behavior. this will change the hotkeys so they move all selected tabs.
+        "Move all selected tabs with hotkeys": true,
+    };
+    class UCMiscMods {
+        constructor() {
+            if (config["Disable bookmarks toolbar auto show"])
+                gEditItemOverlay._autoshowBookmarksToolbar = function () {};
+            if (config["Moving tabs with arrow keys can wrap"]) gBrowser.arrowKeysShouldWrap = true;
+            if (config["Stop downloads panel auto-focusing the footer button"])
+                this.stopDownloadsPanelFocus();
+            if (config["Move all selected tabs with hotkeys"]) this.moveTabKeysMoveSelectedTabs();
+        }
+        stopDownloadsPanelFocus() {
+            eval(
+                `DownloadsPanel._focusPanel = function ` +
+                    DownloadsPanel._focusPanel
+                        .toSource()
+                        .replace(/DownloadsFooter\.focus\(\)\;/, ``)
+            );
+        }
+        moveTabKeysMoveSelectedTabs() {
+            gBrowser.moveTabsBackward = function () {
+                let tabs = this.selectedTab.multiselected ? this.selectedTabs : [this.selectedTab];
+                let previousTab = this.tabContainer.findNextTab(tabs[0], {
+                    direction: -1,
+                    filter: (tab) => !tab.hidden,
+                });
+                for (let tab of tabs) {
+                    if (previousTab) this.moveTabTo(tab, previousTab._tPos);
+                    else if (this.arrowKeysShouldWrap && tab._tPos < this.browsers.length - 1)
+                        this.moveTabTo(tab, this.browsers.length - 1);
+                }
+            };
+            gBrowser.moveTabsForward = function () {
+                let tabs = this.selectedTab.multiselected ? this.selectedTabs : [this.selectedTab];
+                let nextTab = this.tabContainer.findNextTab(tabs[tabs.length - 1], {
+                    direction: 1,
+                    filter: (tab) => !tab.hidden,
+                });
+                for (let i = tabs.length - 1; i >= 0; i--) {
+                    let tab = tabs[i];
+                    if (nextTab) this.moveTabTo(tab, nextTab._tPos);
+                    else if (this.arrowKeysShouldWrap && tab._tPos > 0) this.moveTabTo(tab, 0);
+                }
+            };
+            eval(
+                `gBrowser._handleKeyDownEvent = function ` +
+                    gBrowser._handleKeyDownEvent
+                        .toSource()
+                        .replace(/moveTabBackward/, `moveTabsBackward`)
+                        .replace(/moveTabForward/, `moveTabsForward`)
+            );
+        }
     }
 
-    if (gBrowserInit.delayedStartupFinished) init();
+    if (gBrowserInit.delayedStartupFinished) new UCMiscMods();
     else {
         let delayedListener = (subject, topic) => {
             if (topic == "browser-delayed-startup-finished" && subject == window) {
                 Services.obs.removeObserver(delayedListener, topic);
-                init();
+                new UCMiscMods();
             }
         };
         Services.obs.addObserver(delayedListener, "browser-delayed-startup-finished");
