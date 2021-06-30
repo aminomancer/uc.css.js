5a6b6eb8a3cb7d01adf4a4b1b40b4bdeab36c870
aminomancer <33384265+aminomancer@users.noreply.github.com>
2021-06-30 05:56:21 -0700

    anchor bookmarks/history sidebar tooltip to rows; some adjustments to arrowscrollboxes; change sidebar item padding;

--- script/miscMods.uc.js
+++ script/miscMods.uc.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name           Misc. Mods
-// @version        1.2
+// @version        1.3
 // @author         aminomancer
 // @homepage       https://github.com/aminomancer/uc.css.js
 // @description    Various tiny mods not worth making separate scripts for. Read the comments inside the script for details.
@@ -20,7 +20,7 @@
         // when you use the "move tab" hotkeys, e.g. Ctrl + Shift + PageUp, it only moves the active tab, even if you have multiple tabs selected. this is inconsistent with the keyboard shortcuts "move tab to end" or "move tab to start" and of course, inconsistent with the drag & drop behavior. this will change the hotkeys so they move all selected tabs.
         "Move all selected tabs with hotkeys": true,
 
-        // with browser.proton.places-tooltip.enabled, the bookmarks/history/tabs tooltip is improved and normally it gets anchored to the element that popped up the tooltip, i.e. the element you hovered. but for some reason menupopups are an exception. it does this on all relevant elements, including bookmarks in panels, just not on bookmarks menu popups. but I tested it and it works fine on menupopups so I'm removing the exception.
+        // with browser.proton.places-tooltip.enabled, the bookmarks/history/tabs tooltip is improved and normally it gets anchored to the element that popped up the tooltip, i.e. the element you hovered. but for some reason menupopups are an exception. it does this on all relevant elements, including bookmarks in panels, just not on bookmarks menu popups. but I tested it and it works fine on menupopups so I'm removing the exception. there also isn't any anchoring inside sidebars, because the bookmarks/history items in the sidebar aren't actual DOM elements. there's just one node, the tree, and the individual items are drawn inside it. so they're kind of like virtual nodes. we can't anchor to them the normal way since they're not elements, but we can get their screen coordinates and constrain the tooltip popup within those coordinates. so this will implement the proton places tooltip behavior everywhere, rather than it being restricted to panels and the tab bar.
         "Anchor bookmarks menu tooltip to bookmark": true,
     };
     class UCMiscMods {
@@ -75,12 +75,56 @@
             );
         }
         anchorBookmarksTooltip() {
-            eval(
-                `BookmarksEventHandler.fillInBHTooltip = ` +
-                    BookmarksEventHandler.fillInBHTooltip
-                        .toSource()
-                        .replace(/&&\s*\!tooltipNode\.closest\(\"menupopup\"\)/, ``)
-            );
+            BookmarksEventHandler.fillInBHTooltip = function (aDocument, aEvent) {
+                var node;
+                var cropped = false;
+                var targetURI;
+                if (aDocument.tooltipNode.localName == "treechildren") {
+                    var tree = aDocument.tooltipNode.parentNode;
+                    var cell = tree.getCellAt(aEvent.clientX, aEvent.clientY);
+                    if (cell.row == -1) return false;
+                    node = tree.view.nodeForTreeIndex(cell.row);
+                    cropped = tree.isCellCropped(cell.row, cell.col);
+                    // get coordinates for the cell in a tree.
+                    var cellCoords = tree.getCoordsForCellItem(cell.row, cell.col, "cell");
+                } else {
+                    var tooltipNode = aDocument.tooltipNode;
+                    if (tooltipNode._placesNode) node = tooltipNode._placesNode;
+                    else targetURI = tooltipNode.getAttribute("targetURI");
+                }
+                if (!node && !targetURI) return false;
+                var title = node ? node.title : tooltipNode.label;
+                var url;
+                if (targetURI || PlacesUtils.nodeIsURI(node)) url = targetURI || node.uri;
+                if (!cropped && !url) return false;
+                if (gProtonPlacesTooltip) {
+                    if (tooltipNode) {
+                        aEvent.target.setAttribute("position", "after_start");
+                        aEvent.target.moveToAnchor(tooltipNode, "after_start");
+                    } else if (tree && cellCoords) {
+                        // anchor the tooltip to the tree cell
+                        aEvent.target.moveTo(
+                            cellCoords.left + tree.screenX,
+                            cellCoords.bottom + tree.screenY
+                        );
+                        // make sure the tooltip isn't any bigger than the tree
+                        aEvent.target.sizeTo(cellCoords.width, 0);
+                    }
+                }
+                let tooltipTitle = aEvent.target.querySelector(".places-tooltip-title");
+                tooltipTitle.hidden = !title || title == url;
+                if (!tooltipTitle.hidden) tooltipTitle.textContent = title;
+                let tooltipUrl = aEvent.target.querySelector(".places-tooltip-uri");
+                tooltipUrl.hidden = !url;
+                if (!tooltipUrl.hidden) tooltipUrl.value = url;
+                return true;
+            };
+            // eval(
+            //     `BookmarksEventHandler.fillInBHTooltip = ` +
+            //         BookmarksEventHandler.fillInBHTooltip
+            //             .toSource()
+            //             .replace(/&&\s*\!tooltipNode\.closest\(\"menupopup\"\)/, ``)
+            // );
         }
     }
 
