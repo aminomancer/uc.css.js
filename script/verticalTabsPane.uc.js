94a5e748c60dba3686b05abaad6a531a2686597d
aminomancer <33384265+aminomancer@users.noreply.github.com>
2021-07-03 03:49:43 -0700

    vertical tabs pane: unexpand on close; add sidebar border;

--- script/verticalTabsPane.uc.js
+++ script/verticalTabsPane.uc.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name           Vertical Tabs Pane
-// @version        1.0
+// @version        1.1
 // @author         aminomancer
 // @homepage       https://github.com/aminomancer/uc.css.js
 // @description    Create a vertical pane across from the sidebar that functions like the vertical tab pane in Microsoft Edge. It doesn't hide the tab bar since people have different preferences on how to do that, but it sets an attribute on the root element that you can use to hide the regular tab bar while the vertical pane is open, for example :root[vertical-tabs] #TabsToolbar... By default, the pane is resizable just like the sidebar is. And like the pane in Edge, you can press a button to collapse it, and it will hide the tab labels and become a thin strip that just shows the tabs' favicons. Hovering the collapsed pane will expand it without moving the browser content. As with the [vertical-tabs] attribute, this "unpinned" state is reflected on the root element, so you can select it like :root[vertical-tabs-unpinned]... Like the sidebar, the state of the pane is stored between windows and recorded in preferences. There's no need to edit these preferences directly, but there is one separate preference that's meant to be edited in about:config, userChrome.tabs.verticalTabsPane.reverse-order. This preference changes the direction of the pane so that newer tabs are displayed on top rather than on bottom. There are some other configuration and localization options below.
@@ -405,6 +405,7 @@
             clearTimeout(this.hoverTimer);
             this.hoverOutQueued = false;
             this.hoverQueued = false;
+            this.pane.removeAttribute("expanded");
         }
         _setupListeners() {
             this.listenersRegistered = true;
