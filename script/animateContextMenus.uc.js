e1f3be2f92bcad1d997c34ff143f42a4b2b54aaf
aminomancer <33384265+aminomancer@users.noreply.github.com>
2021-07-08 12:38:13 -0700

    update app menu about:config button for nightly; add "animate context menus" script, which adds the panels' opening animation to context menus.

---
+++ script/animateContextMenus.uc.js
@@ -0,0 +1,75 @@
+// ==UserScript==
+// @name           Animate Context Menus
+// @version        1.0
+// @author         aminomancer
+// @homepage       https://github.com/aminomancer/uc.css.js
+// @description    Give all context menus the same opening animation that panel popups like the app menu have — the menu slides down 70px and fades in opacity at the same time. It's a cool effect that doesn't trigger a reflow since it uses transform, but it does repaint the menu, so I wouldn't recommend using this on weak hardware.
+// @include        *
+// ==/UserScript==
+
+class AnimateContextMenus {
+    constructor() {
+        document.documentElement.setAttribute("animate-menupopups", true);
+        addEventListener("popupshowing", this);
+        addEventListener("popupshown", this);
+        addEventListener("popuphidden", this);
+        let css = `
+:root[animate-menupopups] :not(menulist) > menupopup:not([position], [type="arrow"], [animate="false"]) {
+    opacity: 0;
+    transform: translateY(-70px) scaleX(0.95) scaleY(0.5);
+    transform-origin: top;
+    transition-property: transform, opacity;
+    transition-duration: 0.18s, 0.18s;
+    transition-timing-function:
+        var(--animation-easing-function, cubic-bezier(.07, .95, 0, 1)), ease-out;
+    transform-style: flat;
+    backface-visibility: hidden;
+}
+:root[animate-menupopups] :not(menulist) > menupopup:not([position], [type="arrow"])[animate][animate="open"] {
+    opacity: 1.0;
+    transition-duration: 0.18s, 0.18s;
+    transform: none !important;
+    transition-timing-function:
+        var(--animation-easing-function, cubic-bezier(.07, .95, 0, 1)), ease-in-out;
+}
+:root[animate-menupopups] :not(menulist) > menupopup:not([position], [type="arrow"])[animate][animate="cancel"] {
+    transform: none;
+}
+:root[animate-menupopups] :not(menulist) > menupopup:not([position], [type="arrow"])[animating] {
+    pointer-events: none;
+}
+`;
+        let sss = Cc["@mozilla.org/content/style-sheet-service;1"].getService(
+            Ci.nsIStyleSheetService
+        );
+        let uri = Services.io.newURI("data:text/css;charset=UTF=8," + encodeURIComponent(css));
+        if (!sss.sheetRegistered(uri, sss.AUTHOR_SHEET))
+            sss.loadAndRegisterSheet(uri, sss.AUTHOR_SHEET);
+    }
+    handleEvent(e) {
+        if (e.target.tagName !== "menupopup") return;
+        if (e.target.hasAttribute("position")) return;
+        if (e.target.getAttribute("type") == "arrow") return;
+        if (e.target.parentElement) if (e.target.parentElement.tagName == "menulist") return;
+        if (
+            e.target.shadowRoot &&
+            e.target.shadowRoot.firstElementChild.classList.contains("panel-arrowcontainer")
+        )
+            return;
+        this[`on_${e.type}`](e);
+    }
+    on_popupshowing(e) {
+        if (e.target.getAttribute("animate") != "false") {
+            e.target.setAttribute("animate", "open");
+            e.target.setAttribute("animating", "true");
+        }
+    }
+    on_popupshown(e) {
+        e.target.removeAttribute("animating");
+    }
+    on_popuphidden(e) {
+        if (e.target.getAttribute("animate") != "false") e.target.removeAttribute("animate");
+    }
+}
+
+new AnimateContextMenus();
--- script/appMenuAboutConfigButton.uc.js
+++ script/appMenuAboutConfigButton.uc.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name           App Menu about:config Button
-// @version        1.1
+// @version        1.2
 // @author         aminomancer
 // @homepage       https://github.com/aminomancer/uc.css.js
 // @description    Adds an about:config shortcut button to the main app menu panel, under the built-in Settings button. It can open the built-in about:config page, or it can open the old-school about:config page with earthlng's [aboutconfig](https://github.com/earthlng/aboutconfig) module. To use that with fx-autoconfig, download ONLY the profile/chrome/utils/aboutconfig folder and place it inside your profile/chrome/resources folder. Then open config.xhtml and find & replace "userchromejs" with "userchrome" and save. Now "chrome://userchrome/content/aboutconfig/config.xhtml" should be the correct URL. By default the script will open to that link, so if you don't have that module installed the button will open to a blank page. If you can't get the module to work or if you just prefer Firefox's built-in page, you can change the constant on line 10 below to "about:config" and it'll open to the same page you'd get if you typed about:config in the address bar. (the URL must be in quotes) That said, typing about:config is already easy enough. The reason I made this script was to make a clean shortcut to reach the old-school page, and in a more central location than a bookmark. FYI I added an icon for this button (and for all the other main app menu buttons too) in uc-app-menu.css
@@ -52,9 +52,9 @@
         const advancedPrefsLabel = await configStrings.formatValue(["about-config-page-title"]); // localize the "Advanced Preferences" string
         const { mainView } = PanelUI;
         const doc = mainView.ownerDocument;
-        const settingsButton = PanelUI.protonAppMenuEnabled // if proton is enabled, the buttons used are entirely different nodes
-            ? doc.getElementById("appMenu-settings-button")
-            : doc.getElementById("appMenu-preferences-button");
+        const settingsButton =
+            doc.getElementById("appMenu-settings-button") ??
+            doc.getElementById("appMenu-preferences-button");
         const prefsButton = doc.createXULElement("toolbarbutton");
 
         prefsButton.preferredURL = findAboutConfig();
