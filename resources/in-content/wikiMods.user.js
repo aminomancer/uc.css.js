37aa73704d26b52bf5a2482eae420a3185bd1d30
aminomancer <33384265+aminomancer@users.noreply.github.com>
2021-11-29 03:57:28 -0800

    update wikiMods.user.js

--- resources/in-content/wikiMods.user.js
+++ resources/in-content/wikiMods.user.js
@@ -3,7 +3,7 @@
 // @namespace   https://github.com/aminomancer
 // @match       http*://*.wikipedia.org/wiki/*
 // @grant       none
-// @version     1.0
+// @version     1.1
 // @author      aminomancer
 // @run-at      document-body
 // @description 11/22/2021, 1:26:57 AM
@@ -12,13 +12,21 @@
 class WikiMods {
     constructor() {
         this.logo = this.makeLogo();
+        if (this.logo === "hidden") document.addEventListener('visibilitychange', this);
+    }
+    handleEvent(e) {
+        if (!document.hidden) {
+            this.logo = this.makeLogo();
+            if (typeof this.logo === "object") document.removeEventListener('visibilitychange', this);
+        }
     }
     // add a custom logo, intended to be used in conjunction with my wikipedia dark mode theme:
     // https://github.com/aminomancer/uc.css.js/blob/master/resources/in-content/site-wiki.css
     makeLogo() {
+        if (!location.pathname.startsWith("/wiki/")) return false;
         let original = document.querySelector("#p-logo .mw-wiki-logo");
         let mwHead = document.querySelector("#mw-head");
-        if (!(mwHead && location.pathname.startsWith("/wiki/"))) return false;
+        if (!mwHead) return "hidden";
         let bodyContent = document.querySelector(".mw-body#content");
         bodyContent.setAttribute("uc-custom-logo", true);
         let logo = document.createElement("a");
