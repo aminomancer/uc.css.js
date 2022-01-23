d7580c9a50dadab89bfd8f5ba4621cc9b2d0b0fb
aminomancer <33384265+aminomancer@users.noreply.github.com>
2022-01-23 00:05:58 -0800

    update fullscreen & viewsource for new patches: update toggleMenubarHotkey.uc.js to 1.1.1. update fullscreen menubar styles for this. update viewsource styles for a new patch.

--- JS/toggleMenubarHotkey.uc.js
+++ JS/toggleMenubarHotkey.uc.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name           Toggle Menubar Hotkey
-// @version        1.1
+// @version        1.1.1
 // @author         aminomancer
 // @homepage       https://github.com/aminomancer
 // @description    Press alt+M to toggle the menubar.
@@ -31,112 +31,17 @@
     function init() {
         if (!hotkeyRegistered) return;
         document.getElementById("toolbar-menubar").setAttribute("key", hotkeyId);
-        window.onViewToolbarsPopupShowing = function (aEvent, aInsertPoint) {
-            var popup = aEvent.target;
-            if (popup != aEvent.currentTarget) return;
-            for (var i = popup.children.length - 1; i >= 0; --i) {
-                var deadItem = popup.children[i];
-                if (deadItem.hasAttribute("toolbarId")) popup.removeChild(deadItem);
-            }
-            MozXULElement.insertFTLIfNeeded("browser/toolbarContextMenu.ftl");
-            let firstMenuItem = aInsertPoint || popup.firstElementChild;
-            let toolbarNodes = gNavToolbox.querySelectorAll("toolbar");
-            for (let toolbar of toolbarNodes) {
-                if (!toolbar.hasAttribute("toolbarname")) continue;
-
-                if (toolbar.id == "PersonalToolbar") {
-                    let menu = BookmarkingUI.buildBookmarksToolbarSubmenu(toolbar);
-                    popup.insertBefore(menu, firstMenuItem);
-                } else {
-                    let menuItem = document.createXULElement("menuitem");
-                    menuItem.setAttribute("id", "toggle_" + toolbar.id);
-                    menuItem.setAttribute("toolbarId", toolbar.id);
-                    menuItem.setAttribute("type", "checkbox");
-                    menuItem.setAttribute("label", toolbar.getAttribute("toolbarname"));
-                    let hidingAttribute =
-                        toolbar.getAttribute("type") == "menubar" ? "autohide" : "collapsed";
-                    menuItem.setAttribute(
-                        "checked",
-                        toolbar.getAttribute(hidingAttribute) != "true"
-                    );
-                    menuItem.setAttribute("accesskey", toolbar.getAttribute("accesskey"));
-                    if (toolbar.hasAttribute("key"))
-                        menuItem.setAttribute("key", toolbar.getAttribute("key"));
-                    popup.insertBefore(menuItem, firstMenuItem);
-                    menuItem.addEventListener("command", onViewToolbarCommand);
-                }
-            }
-            let moveToPanel = popup.querySelector(".customize-context-moveToPanel");
-            let removeFromToolbar = popup.querySelector(".customize-context-removeFromToolbar");
-            if (!moveToPanel || !removeFromToolbar) return;
-            let toolbarItem = popup.triggerNode;
-            if (toolbarItem && toolbarItem.localName == "toolbarpaletteitem")
-                toolbarItem = toolbarItem.firstElementChild;
-            else if (toolbarItem && toolbarItem.localName != "toolbar")
-                while (toolbarItem && toolbarItem.parentElement) {
-                    let parent = toolbarItem.parentElement;
-                    if (
-                        (parent.classList && parent.classList.contains("customization-target")) ||
-                        parent.getAttribute("overflowfortoolbar") ||
-                        parent.localName == "toolbarpaletteitem" ||
-                        parent.localName == "toolbar"
-                    )
-                        break;
-                    toolbarItem = parent;
-                }
-            else toolbarItem = null;
-            let showTabStripItems = toolbarItem && toolbarItem.id == "tabbrowser-tabs";
-            for (let node of popup.querySelectorAll('menuitem[contexttype="toolbaritem"]'))
-                node.hidden = showTabStripItems;
-            for (let node of popup.querySelectorAll('menuitem[contexttype="tabbar"]'))
-                node.hidden = !showTabStripItems;
-            document
-                .getElementById("toolbar-context-menu")
-                .querySelectorAll("[data-lazy-l10n-id]")
-                .forEach((el) => {
-                    el.setAttribute("data-l10n-id", el.getAttribute("data-lazy-l10n-id"));
-                    el.removeAttribute("data-lazy-l10n-id");
-                });
-            let menuSeparator = document.getElementById("toolbarItemsMenuSeparator");
-            menuSeparator.hidden = false;
-            document.getElementById("toolbarNavigatorItemsMenuSeparator").hidden =
-                !showTabStripItems;
-            if (
-                !CustomizationHandler.isCustomizing() &&
-                CustomizableUI.isSpecialWidget(toolbarItem?.id || "")
-            ) {
-                moveToPanel.hidden = true;
-                removeFromToolbar.hidden = true;
-                menuSeparator.hidden = !showTabStripItems;
-            }
-            if (showTabStripItems) {
-                let multipleTabsSelected = !!gBrowser.multiSelectedTabsCount;
-                document.getElementById("toolbar-context-bookmarkSelectedTabs").hidden =
-                    !multipleTabsSelected;
-                document.getElementById("toolbar-context-bookmarkSelectedTab").hidden =
-                    multipleTabsSelected;
-                document.getElementById("toolbar-context-reloadSelectedTabs").hidden =
-                    !multipleTabsSelected;
-                document.getElementById("toolbar-context-reloadSelectedTab").hidden =
-                    multipleTabsSelected;
-                document.getElementById("toolbar-context-selectAllTabs").disabled =
-                    gBrowser.allTabsSelected();
-                document.getElementById("toolbar-context-undoCloseTab").disabled =
-                    SessionStore.getClosedTabCount(window) == 0;
-                return;
-            }
-            let movable =
-                toolbarItem && toolbarItem.id && CustomizableUI.isWidgetRemovable(toolbarItem);
-            if (movable) {
-                if (CustomizableUI.isSpecialWidget(toolbarItem.id))
-                    moveToPanel.setAttribute("disabled", true);
-                else moveToPanel.removeAttribute("disabled");
-                removeFromToolbar.removeAttribute("disabled");
-            } else {
-                moveToPanel.setAttribute("disabled", true);
-                removeFromToolbar.setAttribute("disabled", true);
-            }
-        };
+        let src = onViewToolbarsPopupShowing.toSource();
+        if (src.startsWith("function"))
+            eval(
+                `window.onViewToolbarsPopupShowing = function uc_onViewToolbarsPopupShowing ` +
+                    src
+                        .replace(/^function onViewToolbarsPopupShowing/, "")
+                        .replace(
+                            /if \(popup\.id != \"toolbar-context-menu\"\)/,
+                            `if (toolbar.hasAttribute("key"))`
+                        )
+            );
     }
 
     if (gBrowserInit.delayedStartupFinished) {
