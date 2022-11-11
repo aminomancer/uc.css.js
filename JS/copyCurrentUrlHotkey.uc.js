8a278008c7aeab3f1176ff02fa91f8a31004ad25
Shane Hughes <shughes@mozilla.com>
2022-11-11 11:40:09 -0800

    Update copyCurrentUrlHotkey.uc.js and some CSS.
    (JS) Update copyCurrentUrlHotkey.uc.js to correctly encode/decode the
    URL based on user prefs.
    (CSS) Change the focused background for ctrlTab previews to be more
    visible when the screen is dark.
    Fix the margins of permission popups, identity popup, etc.

--- JS/copyCurrentUrlHotkey.uc.js
+++ JS/copyCurrentUrlHotkey.uc.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name           Copy Current URL Hotkey
-// @version        1.2.1
+// @version        1.2.2
 // @author         aminomancer
 // @homepage       https://github.com/aminomancer
 // @description    Adds a new hotkey (Ctrl+Alt+C by default) that copies
@@ -13,19 +13,6 @@ class CopyCurrentURL {
     // if you have customHintProvider.uc.js, copying will open a confirmation
     // hint anchored to the urlbar.
     "copy confirmation hint": true,
-
-    // when you right-click the urlbar, the context menu has a "copy" command.
-    // set this to "true" to show a "Ctrl+Alt+C" hint next to this command, like
-    // firefox does with many other commands. the hint text will reflect the
-    // actual hotkey. so on macOS it will show "Cmd+Alt+C" and if you modify the
-    // modifiers below, it will show your modifiers instead. this setting isn't
-    // enabled by default because 1) unlike our custom hotkey, this command
-    // actually only copies the selection, not the full input content. so it's
-    // disabled if nothing is highlighted. and 2) the context menu is very thin
-    // due to the short names of the commands. adding "Ctrl+Alt+C" makes it kind
-    // of cramped. but it's easy to forget that hotkeys exist if they're not
-    // visually displayed anywhere, so you may want to enable this feature.
-    "context menu shortcut hint": true,
 
     shortcut: {
       // shortcut key, combined with modifiers.
@@ -43,6 +30,7 @@ class CopyCurrentURL {
       id: "key_copyCurrentUrl",
     },
   };
+
   constructor() {
     XPCOMUtils.defineLazyServiceGetter(
       this,
@@ -52,59 +40,43 @@ class CopyCurrentURL {
     );
     this.hotkey = _ucUtils.registerHotkey(this.config.shortcut, win => {
       if (win === window) {
-        let val = win.gURLBar._lastValidURLStr || win.gURLBar.value;
-        if (!val) return;
+        let val;
+        try {
+          let uri = win.gURLBar.makeURIReadable(win.gBrowser.currentURI);
+          if (uri.schemeIs("javascript") || uri.schemeIs("data")) {
+            val = win.gURLBar._lastValidURLStr || win.gURLBar.value;
+          } else {
+            val = uri.displaySpec;
+          }
+          if (win.UrlbarPrefs.get("decodeURLsOnCopy")) {
+            val = decodeURI(val);
+          }
+        } catch (error) {
+          return;
+        }
         this.ClipboardHelper.copyStringToClipboard(val, this.clipboard);
         if (this.config["copy confirmation hint"]) {
-          win.CustomHint?.show(win.gURLBar.inputField, "Copied", {
-            position: "after_start",
-            x: 16,
-          });
+          if (win.gURLBar.getAttribute("pageproxystate") == "valid") {
+            win.CustomHint?.show(win.gURLBar.inputField, "Copied", {
+              position: "after_start",
+              x: 16,
+            });
+          } else {
+            win.CustomHint?.show(
+              win.gIdentityHandler._identityIconBox,
+              "Copied",
+              { position: "bottomcenter topleft", y: 8 }
+            );
+          }
         }
       }
     });
-    if (this.config["context menu shortcut hint"]) this.shortcutHint();
   }
+
   get clipboard() {
     return Services.clipboard.supportsSelectionClipboard()
       ? Services.clipboard.kSelectionClipboard
       : Services.clipboard.kGlobalClipboard;
-  }
-  handleEvent() {
-    let menuitem = gURLBar.inputField?.parentElement?.menupopup?.querySelector(
-      `[cmd="cmd_copy"]`
-    );
-    if (menuitem) {
-      if (!this.hintApplied && menuitem.hasAttribute("key")) {
-        gURLBar.removeEventListener("contextmenu", this);
-        return;
-      }
-      if (
-        gURLBar.selectionStart != 0 ||
-        gURLBar.selectionEnd != gURLBar.inputField.textLength
-      ) {
-        menuitem.setAttribute("key", this.config.shortcut.id);
-      } else {
-        menuitem.removeAttribute("key");
-      }
-      this.hintApplied = true;
-    }
-  }
-  shortcutHint() {
-    if (gBrowserInit.delayedStartupFinished) {
-      gURLBar.addEventListener("contextmenu", this);
-    } else {
-      let delayedListener = (subject, topic) => {
-        if (topic == "browser-delayed-startup-finished" && subject == window) {
-          Services.obs.removeObserver(delayedListener, topic);
-          gURLBar.addEventListener("contextmenu", this);
-        }
-      };
-      Services.obs.addObserver(
-        delayedListener,
-        "browser-delayed-startup-finished"
-      );
-    }
   }
 }
 
