3f460bc06968bfaf797f77f9b8237a5ef1b63ada
Shane Hughes <shughes@mozilla.com>
2023-01-22 07:37:30 -0800

    Fix copyCurrentUrlHotkey.uc.js. Fix urlbar results appearance.

--- JS/copyCurrentUrlHotkey.uc.js
+++ JS/copyCurrentUrlHotkey.uc.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name           Copy Current URL Hotkey
-// @version        1.2.3
+// @version        1.2.4
 // @author         aminomancer
 // @homepageURL    https://github.com/aminomancer
 // @description    Adds a new hotkey (Ctrl+Alt+C by default) that copies whatever is in the urlbar, even when it's not in focus.
@@ -10,14 +10,19 @@
 // ==/UserScript==
 
 class CopyCurrentURL {
+  // Add these in about:config instead of modifying the script, if you want the
+  // settings to persist between script restarts.
   config = {
     // if you have customHintProvider.uc.js, copying will open a confirmation
     // hint anchored to the urlbar.
-    "copy confirmation hint": true,
+    "show confirmation hint": Services.prefs.getBoolPref(
+      "copyCurrentUrl.showConfirmationHint",
+      true
+    ),
 
     shortcut: {
       // shortcut key, combined with modifiers.
-      key: "C",
+      key: Services.prefs.getStringPref("copyCurrentUrl.shortcut.key", "C"),
 
       // ctrl + alt or cmd + alt (use accel, it's cross-platform. it can be
       // changed in about:config with ui.key.accelKey. if you leave the "" quotes
@@ -25,7 +30,10 @@ class CopyCurrentURL {
       // which is a bad idea — only do that if your "key" value is something
       // obscure like a function key, since this key will be active at all times
       // and in almost all contexts.
-      modifiers: "accel alt",
+      modifiers: Services.prefs.getStringPref(
+        "copyCurrentUrl.shortcut.modifiers",
+        "accel alt"
+      ),
 
       // no need to change this.
       id: "key_copyCurrentUrl",
@@ -56,7 +64,7 @@ class CopyCurrentURL {
           return;
         }
         this.ClipboardHelper.copyStringToClipboard(val, this.clipboard);
-        if (this.config["copy confirmation hint"]) {
+        if (this.config["show confirmation hint"]) {
           if (win.gURLBar.getAttribute("pageproxystate") == "valid") {
             win.CustomHint?.show(win.gURLBar.inputField, "Copied", {
               position: "after_start",
@@ -75,7 +83,9 @@ class CopyCurrentURL {
   }
 
   get clipboard() {
-    return Services.clipboard.supportsSelectionClipboard()
+    return Services.clipboard.isClipboardTypeSupported(
+      Services.clipboard.kSelectionClipboard
+    )
       ? Services.clipboard.kSelectionClipboard
       : Services.clipboard.kGlobalClipboard;
   }
