aeceba502c1925e9863ffa653c57490e66cee926
Max <maximtsyba@gmail.com>
2023-07-23 01:23:12 +0300

    Add comment and some padding to make sure effect clears out

--- JS/fluentRevealNavbar.uc.js
+++ JS/fluentRevealNavbar.uc.js
@@ -84,7 +84,9 @@
      * @param {object} e (event)
      */
     handleEvent(e) {
-      if (this._options.filterDy && e.clientY > this._options.gradientSize) return;
+      /// filter out mouse events which are too far from toolbar to cause any actual redraw
+      /// value is {gradientSize} + some additional padding to make sure effect clears out properly
+      if (this._options.filterDy && e.clientY > this._options.gradientSize + 20) return;
       
       requestAnimationFrame(time => {
         switch (e.type) {
